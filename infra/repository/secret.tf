ba14381a89f51f81eb1da81da1a88426544d34a1
Mkamono <chengyaye31@gmail.com>
2024-07-14 22:34:36 +0900

    add secret

--- infra/repository/secret.tf
+++ infra/repository/secret.tf
@@ -1,5 +1,11 @@
-resource "github_actions_secret" "workload_identity_pool_provider_id" {
+resource "github_actions_secret" "wif_pool_provider_id" {
   repository      = github_repository.share-cart.name
   secret_name     = "WIF_POOL_PROVIDER_ID"
   plaintext_value = google_iam_workload_identity_pool_provider.pool-provider.id
+}
+
+resource "github_actions_secret" "wif_sa_email" {
+  repository      = github_repository.share-cart.name
+  secret_name     = "WIF_SA_EMAIL"
+  plaintext_value = google_service_account.wif_sa.email
 }
