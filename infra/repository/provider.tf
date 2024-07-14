045bcec2199afa4f304da2ed75fdf5a96f05a4e7
Mkamono <chengyaye31@gmail.com>
2024-07-14 10:08:24 +0000

    set backend

--- infra/repository/provider.tf
+++ infra/repository/provider.tf
@@ -1,12 +1,21 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     github = {
       source  = "integrations/github"
       version = "~> 6.0"
     }
+    google = {
+      source  = "hashicorp/google"
+      version = "5.37.0"
+    }
   }
 
-  required_version = ">= 1.5.0"
+  backend "gcs" {
+    bucket = "kamono-terraform-state-bucket"
+    prefix = "terraform/state"
+  }
 }
 
 provider "github" {
