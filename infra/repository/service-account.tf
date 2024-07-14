835babbcc8004419e6e66ea2528f8ab7a7f9a99a
Mkamono <chengyaye31@gmail.com>
2024-07-14 13:27:33 +0000

    fix service account

--- infra/repository/service-account.tf
+++ infra/repository/service-account.tf
@@ -6,5 +6,5 @@ resource "google_service_account" "wif_sa" {
 resource "google_service_account_iam_member" "wif_sa_member" {
   service_account_id = google_service_account.wif_sa.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${google_service_account.wif_sa.email}"
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.gh_identity_pool.name}/attribute.repository/${github_repository.share-cart.full_name}"
 }
