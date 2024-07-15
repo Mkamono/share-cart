4628b259e9917cdefadf95fb027bc675b052caa0
Mkamono <chengyaye31@gmail.com>
2024-07-15 00:07:09 +0000

    delete

--- infra/repository/service-account.tf
+++ infra/repository/service-account.tf
@@ -3,37 +3,21 @@ resource "google_service_account" "wif_sa" {
   display_name = "WIF Service Account"
 }
 
-# data "google_iam_policy" "admin" {
-#   binding {
-#     role = "roles/iam.serviceAccountUser"
-
-#     members = []
-#   }
+# resource "google_service_account_iam_member" "wif_sa_iam_wif_user" {
+#   service_account_id = google_service_account.wif_sa.id
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = "serviceAccount:${data.google_project.project.number}.svc.id.goog[default/gh-pool]"
 # }
 
-resource "google_service_account_iam_member" "wif_sa_iam" {
-  service_account_id = google_service_account.wif_sa.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:${data.google_project.project.number}.svc.id.goog[default/gh-pool]"
-}
-
-
-# data "google_iam_policy" "admin" {
-#   binding {
-#     role = "roles/iam.serviceAccountUser"
-
-#     members = [
-#       "user:jane@example.com",
-#     ]
-#   }
+# resource "google_service_account_iam_member" "wif_sa_iam_editor" {
+#   service_account_id = google_service_account.wif_sa.id
+#   role               = "roles/editor"
+#   member             = "serviceAccount:${data.google_project.project.number}.svc.id.goog[default/gh-pool]"
 # }
 
-# resource "google_service_account" "sa" {
-#   account_id   = "my-service-account"
-#   display_name = "A service account that only Jane can interact with"
-# }
 
-# resource "google_service_account_iam_policy" "admin-account-iam" {
-#   service_account_id = google_service_account.sa.name
-#   policy_data        = data.google_iam_policy.admin.policy_data
+# resource "google_service_account_iam_member" "wif_sa_iam" {
+#   service_account_id = google_service_account.wif_sa.name
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = "serviceAccount:${data.google_project.project.number}.svc.id.goog[default/gh-pool]"
 # }
