b148ebb9333c4e9ee6b34fe665dabea73431e095
Mkamono <chengyaye31@gmail.com>
2024-07-15 09:23:10 +0000

    test revert

--- infra/repository/oidc.tf
+++ infra/repository/oidc.tf
@@ -1,18 +1,18 @@
-module "gh_oidc" {
-  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
-  project_id  = data.google_project.project.project_id
-  pool_id     = "gh-pool"
-  provider_id = "gh-provider"
-  sa_mapping = {
-    "foo-service-account" = {
-      sa_name   = google_service_account.wif_sa.name
-      attribute = "attribute.repository/${github_repository.share-cart.full_name}"
-    }
-  }
-}
+# module "gh_oidc" {
+#   source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+#   project_id  = data.google_project.project.project_id
+#   pool_id     = "gh-pool"
+#   provider_id = "gh-provider"
+#   sa_mapping = {
+#     "foo-service-account" = {
+#       sa_name   = google_service_account.wif_sa.name
+#       attribute = "attribute.repository/${github_repository.share-cart.full_name}"
+#     }
+#   }
+# }
 
-data "google_iam_workload_identity_pool_provider" "wif_pool_provider" {
-  provider                           = google-beta
-  workload_identity_pool_id          = "gh-pool"
-  workload_identity_pool_provider_id = "gh-provider"
-}
+# data "google_iam_workload_identity_pool_provider" "wif_pool_provider" {
+#   provider                           = google-beta
+#   workload_identity_pool_id          = "gh-pool"
+#   workload_identity_pool_provider_id = "gh-provider"
+# }
