9fa8d1027c9115995a7762902f1e71f88c16ef65
Mkamono <chengyaye31@gmail.com>
2024-07-20 08:22:28 +0000

    update

--- infra/repository/service-account.tf
+++
@@ -1,4 +0,0 @@
-resource "google_service_account" "gha_wif_sa" {
-  account_id   = "gha-wif-sa"
-  display_name = "WIF Service Account for GitHub Actions"
-}
---
+++ infra/terraform/oidc.tf
@@ -0,0 +1,12 @@
+# module "gh_oidc" {
+#   source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+#   project_id  = local.project_id
+#   pool_id     = "gha-pool"
+#   provider_id = "gha-provider"
+#   sa_mapping = {
+#     (google_service_account.gha_wif_sa.account_id) = {
+#       sa_name   = google_service_account.gha_wif_sa.name
+#       attribute = "attribute.repository/${github_repository.share-cart.full_name}"
+#     }
+#   }
+# }
