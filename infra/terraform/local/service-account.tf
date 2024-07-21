cb3951bada533684f59d168f38ffb981d09e3f69
Mkamono <chengyaye31@gmail.com>
2024-07-21 11:30:22 +0900

    add role

--- infra/terraform/local/service-account.tf
+++ infra/terraform/local/service-account.tf
@@ -2,3 +2,9 @@ resource "google_service_account" "gha_wif_sa" {
   account_id   = "gha-wif-sa"
   display_name = "WIF Service Account for GitHub Actions"
 }
+
+resource "google_storage_bucket_iam_member" "gha_wif_sa_iam" {
+  bucket = google_storage_bucket.terraform_state_bucket.name
+  role   = "roles/storage.objectUser"
+  member = "serviceAccount:${google_service_account.gha_wif_sa.email}"
+}
