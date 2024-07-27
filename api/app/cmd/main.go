f1c6471155cfd2a861eba49f440ab9f90e8e7894
Mkamono <chengyaye31@gmail.com>
2024-07-27 08:30:38 +0000

    delete open api

--- api/app/cmd/main.go
+++ api/app/cmd/main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"api/app/config"
-	"api/app/server"
 	"fmt"
 	"log"
-
-	gen "api/openapi"
-
-	mw "api/app/middleware"
 
 	"github.com/labstack/echo/v4"
 )
@@ -19,12 +14,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	server := server.NewServer()
-
 	e := echo.New()
-	e.Use(mw.Logger(mw.SlogLoggerFunc))
-
-	gen.RegisterHandlers(e, server)
 
 	log.Fatal(e.Start(fmt.Sprintf("0.0.0.0:%s", c.Port)))
 }
