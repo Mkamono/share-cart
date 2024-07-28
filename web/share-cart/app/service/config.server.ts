3af0b3c5e12d1eaaf3db544cdebb0405a1891d79
Mkamono <chengyaye31@gmail.com>
2024-07-29 00:59:44 +0900

    add env

---
+++ web/share-cart/app/service/config.server.ts
@@ -0,0 +1,12 @@
+import { cleanEnv, str, url } from "envalid";
+
+export const config = cleanEnv(process.env, {
+	SESSION_SECRET: str(),
+	AUTH0_CALLBACK_URL: url(),
+	AUTH0_CLIENT_ID: str(),
+	AUTH0_CLIENT_SECRET: str(),
+	AUTH0_DOMAIN: str(),
+	AUTH0_LOGOUT_URL: url(),
+	AUTH0_RETURN_TO_URL: url(),
+	NODE_ENV: str({ choices: ["development", "production", "staging", "test"] }),
+});
