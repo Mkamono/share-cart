2fc0ecc5d403d5cc698c417b01cc7e7f962e76f9
Kamono Makoto <77344894+Mkamono@users.noreply.github.com>
2024-07-28 13:11:42 +0900

    feat: biome (#14)
    * biome
    
    * delete eslint
    
    * enable format on save
    
    * update dev env

--- web/share-cart/app/routes/_index.tsx
+++ web/share-cart/app/routes/_index.tsx
@@ -1,48 +1,61 @@
-import type { MetaFunction } from "@remix-run/node";
+import { type MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+type LoaderData = {
+	message: string;
+};
+
+export async function loader() {
+	const response = await fetch(`${process.env.API_HOST}/test`);
+	const data: LoaderData = await response.json();
+	return json(data);
+}
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: "New Remix App" },
+		{ name: "description", content: "Welcome to Remix!" },
+	];
 };
 
 export default function Index() {
-  return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	const data = useLoaderData<typeof loader>();
+	return (
+		<div className="font-sans p-4">
+			<h1 className="text-3xl">Welcome to Remix</h1>
+			<ul className="list-disc mt-4 pl-6 space-y-2">
+				<li>
+					<a
+						className="text-blue-700 underline visited:text-purple-900"
+						target="_blank"
+						href="https://remix.run/start/quickstart"
+						rel="noreferrer"
+					>
+						5m Quick Start
+					</a>
+					<p>{data.message}</p>
+				</li>
+				<li>
+					<a
+						className="text-blue-700 underline visited:text-purple-900"
+						target="_blank"
+						href="https://remix.run/start/tutorial"
+						rel="noreferrer"
+					>
+						30m Tutorial
+					</a>
+				</li>
+				<li>
+					<a
+						className="text-blue-700 underline visited:text-purple-900"
+						target="_blank"
+						href="https://remix.run/docs"
+						rel="noreferrer"
+					>
+						Remix Docs
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 }
