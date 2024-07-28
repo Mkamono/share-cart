999ba543fbb6ccf8dbc362c58adb88f7109aa8f6
Mkamono <chengyaye31@gmail.com>
2024-07-29 01:00:12 +0900

    add auth page

---
+++ web/share-cart/app/routes/auth.auth0.callback.tsx
@@ -0,0 +1,10 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+
+import { authenticator } from "../service/auth.server";
+
+export const loader = ({ request }: LoaderFunctionArgs) => {
+	return authenticator.authenticate("auth0", request, {
+		successRedirect: "/",
+		failureRedirect: "/login",
+	});
+};
