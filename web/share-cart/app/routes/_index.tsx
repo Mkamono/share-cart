e6f76e6d72374eb849708f0d6bd7a087859e3244
Kamono Makoto <77344894+Mkamono@users.noreply.github.com>
2024-07-29 21:48:48 +0900

    feat: remix auth (#16)

--- web/share-cart/app/routes/_index.tsx
+++ web/share-cart/app/routes/_index.tsx
@@ -2,11 +2,23 @@ import { type MetaFunction, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import createClient from "openapi-fetch";
 import type { paths } from "~/models/schema";
+import { authenticator } from "~/service/auth.server";
+import { config } from "~/service/config.server";
+import { getSession } from "~/service/session.server";
+import Login from "./login";
 
-export async function loader() {
+export async function loader({ request }: { request: Request }) {
+	const user = await authenticator.isAuthenticated(request);
+	const email = user?.profile.emails?.[0].value;
+	const session = await getSession();
 	const client = createClient<paths>({ baseUrl: process.env.API_HOST });
 	const { data } = await client.GET("/test", {});
-	return json(data);
+	return json({
+		testData: data,
+		config,
+		session,
+		user,
+	});
 }
 
 export const meta: MetaFunction = () => {
@@ -23,35 +35,23 @@ export default function Index() {
 			<h1 className="text-3xl">Welcome to Remix</h1>
 			<ul className="list-disc mt-4 pl-6 space-y-2">
 				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/start/quickstart"
-						rel="noreferrer"
-					>
-						5m Quick Start
-					</a>
-					<p>{data.message}</p>
-				</li>
-				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/start/tutorial"
-						rel="noreferrer"
-					>
-						30m Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						className="text-blue-700 underline visited:text-purple-900"
-						target="_blank"
-						href="https://remix.run/docs"
-						rel="noreferrer"
-					>
-						Remix Docs
-					</a>
+					<p>{data.testData?.message}</p>
+					{data.config.NODE_ENV === "development" && (
+						<p className="text-red-700">You are in development mode</p>
+					)}
+					<Login />
+					{data.session ? (
+						<div>
+							<p className="text-green-700">You are logged in</p>
+							<p>{data.user?.profile.id}</p>
+							<p>{data.user?.profile.displayName}</p>
+							{data.user?.profile.emails?.map((email) => (
+								<p key={email.value}>{email.value}</p>
+							))}
+						</div>
+					) : (
+						<p className="text-red-700">You are not logged in</p>
+					)}
 				</li>
 			</ul>
 		</div>
