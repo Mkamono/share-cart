6d2fe8c64f8dcdec36ef3ae33001d0b06f16a0a5
Kamono Makoto <77344894+Mkamono@users.noreply.github.com>
2024-08-08 22:50:57 +0900

    Fix various code formatting issues (#44)
    fix

--- web/share-cart/app/routes/auth/login/index.tsx
+++ web/share-cart/app/routes/auth/login/index.tsx
@@ -2,9 +2,9 @@ import { Form } from "@remix-run/react";
 import { $path } from "remix-routes";
 
 export default function Login() {
-    return (
-        <Form action={$path("/auth/auth0/login")} method="post">
-            <button type="submit">Login with Auth0</button>
-        </Form>
-    );
+	return (
+		<Form action={$path("/auth/auth0/login")} method="post">
+			<button type="submit">Login with Auth0</button>
+		</Form>
+	);
 }
--- web/share-cart/app/routes/auth/logout/index.tsx
+++ web/share-cart/app/routes/auth/logout/index.tsx
@@ -2,9 +2,9 @@ import { Form } from "@remix-run/react";
 import { $path } from "remix-routes";
 
 export default function Logout() {
-    return (
-        <Form action={$path("/auth/auth0/logout")} method="post">
-            <button type="submit">Logout with Auth0</button>
-        </Form>
-    );
+	return (
+		<Form action={$path("/auth/auth0/logout")} method="post">
+			<button type="submit">Logout with Auth0</button>
+		</Form>
+	);
 }
