9bc02cb2c72d4b00e47e0db02288f097916cfc2f
Kamono Makoto <77344894+Mkamono@users.noreply.github.com>
2024-08-14 01:30:56 +0900

    chore: cardのrefactor (#70)
    fix

--- web/share-cart/app/routes/_top/home/MarketCard.tsx
+++ web/share-cart/app/routes/_top/home/MarketCard.tsx
@@ -1,5 +1,4 @@
-import * as Avatar from "@radix-ui/react-avatar";
-import { Card, Inset, Strong, Text } from "@radix-ui/themes";
+import { Avatar, Card, Inset, Strong, Text } from "@radix-ui/themes";
 
 type MarketCardProps = {
 	market: {
@@ -10,38 +9,25 @@ type MarketCardProps = {
 	};
 };
 
+const noImageURL =
+	"https://placehold.jp/eeeeee/cccccc/240x140.png?text=No%20Image";
+
 export const MarketCard = (props: MarketCardProps) => {
 	return (
 		<Card size="2">
 			<Inset clip="padding-box" side="top" pb="current" className="h-44">
-				<Avatar.Root className="AvatarRoot">
-					<Avatar.Image
-						className="AvatarImage"
-						src={props.market.imageURL}
-						alt={props.market.name}
-						style={{
-							display: "block",
-							objectFit: "cover",
-							width: "100%",
-							height: "100%",
-							backgroundColor: "var(--gray-5)",
-						}}
-					/>
-					<Avatar.Fallback className="AvatarFallback" delayMs={500}>
+				<Avatar
+					src={props.market.imageURL}
+					alt={props.market.name}
+					fallback={
 						<img
-							className="AvatarImage"
-							src="https://placehold.jp/eeeeee/cccccc/240x180.png?text=No%20Image"
+							src={noImageURL}
 							alt={props.market.name}
-							style={{
-								display: "block",
-								objectFit: "cover",
-								width: "100%",
-								height: "100%",
-								backgroundColor: "var(--gray-5)",
-							}}
+							className="w-full h-full object-cover"
 						/>
-					</Avatar.Fallback>
-				</Avatar.Root>
+					}
+					className="w-full h-full object-cover"
+				/>
 			</Inset>
 			<Text as="p" size="2">
 				<Strong>{props.market.name}</Strong>
