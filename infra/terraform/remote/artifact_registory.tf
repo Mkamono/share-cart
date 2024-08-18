6705b46087b960594b9984eb5241d5e1cff7ff87
Mkamono <chengyaye31@gmail.com>
2024-08-18 10:14:59 +0900

    add infra

--- infra/terraform/remote/artifact_registory.tf
+++ infra/terraform/remote/artifact_registory.tf
@@ -4,3 +4,10 @@ resource "google_artifact_registry_repository" "api-repo" {
   description   = "share cart api repository"
   format        = "DOCKER"
 }
+
+resource "google_artifact_registry_repository" "web-repo" {
+  location      = local.region
+  repository_id = "web-repo"
+  description   = "share cart web repository"
+  format        = "DOCKER"
+}
--- infra/terraform/remote/croud_run.tf
+++ infra/terraform/remote/croud_run.tf
@@ -6,6 +6,10 @@ resource "google_cloud_run_v2_service" "api" {
 
   template {
     service_account = google_service_account.cloud_run_service_account.email
+    scaling {
+      max_instance_count = 3
+      min_instance_count = 0
+    }
     containers {
       image = "asia-northeast1-docker.pkg.dev/coastal-fiber-430001-s0/api-repo/api:latest"
       resources {
@@ -14,6 +18,9 @@ resource "google_cloud_run_v2_service" "api" {
           cpu    = "2"
           memory = "512Mi"
         }
+      }
+      ports {
+        container_port = 8080
       }
 
       // シークレットの設定
@@ -76,6 +83,107 @@ resource "google_cloud_run_v2_service" "api" {
         value_source {
           secret_key_ref {
             secret  = google_secret_manager_secret.auth0_audience.secret_id
+            version = "latest"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_v2_service" "web" {
+  name     = "web"
+  location = local.region
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    service_account = google_service_account.cloud_run_service_account.email
+    scaling {
+      max_instance_count = 3
+      min_instance_count = 0
+    }
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      resources {
+        cpu_idle = true
+        limits = {
+          cpu    = "2"
+          memory = "512Mi"
+        }
+      }
+      ports {
+        container_port = 3000
+      }
+
+      // シークレットの設定
+      env {
+        name = "SESSION_SECRET"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.auth0_session_secret.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AUTH0_CALLBACK_URL"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.auth0_callback_url.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AUTH0_CLIENT_ID"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.auth0_client_id.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AUTH0_CLIENT_SECRET"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.auth0_client_secret.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AUTH0_DOMAIN"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.auth0_domain.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AUTH0_LOGOUT_URL"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.auth0_logout_url.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AUTH0_AUDIENCE"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.auth0_audience.secret_id
+            version = "latest"
+          }
+        }
+      }
+      env {
+        name = "AUTH0_RETURN_TO_URL"
+        value_source {
+          secret_key_ref {
+            secret  = google_secret_manager_secret.auth0_return_to_url.secret_id
             version = "latest"
           }
         }
