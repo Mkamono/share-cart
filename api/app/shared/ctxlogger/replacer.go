150ee2926814b3d573cd8a7ca7cd694e55309e7d
Mkamono <chengyaye31@gmail.com>
2024-09-01 21:05:22 +0900

    refactor

--- api/app/shared/ctxlogger/replacer.go
+++ api/app/shared/ctxlogger/replacer.go
@@ -3,16 +3,15 @@ package ctxlogger
 import "log/slog"
 
 func GoogleCloudReplacer(groups []string, a slog.Attr) slog.Attr {
-	if a.Key == slog.LevelKey {
+	switch a.Key {
+	case slog.LevelKey:
 		if a.Key == slog.LevelWarn.String() {
-			slog.String("severity", "WARNING")
+			return slog.String("severity", "WARNING")
 		}
 		a.Key = "severity"
-	}
-	if a.Key == slog.MessageKey {
+	case slog.MessageKey:
 		a.Key = "message"
-	}
-	if a.Key == slog.SourceKey {
+	case slog.SourceKey:
 		a.Key = "logging.googleapis.com/sourceLocation"
 	}
 	return a
