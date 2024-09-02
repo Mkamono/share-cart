8225b7400db1feac4076d3fda521cadcaabbb766
Mkamono <chengyaye31@gmail.com>
2024-09-03 01:27:14 +0900

    レビュー対応

--- web/share-cart/app/routes/_top/home/MarketCard.tsx
+++ web/share-cart/app/routes/_top/home/MarketCard.tsx
@@ -6,6 +6,7 @@ type MarketCardProps = {
 		description: string;
 	};
 };
+const defaultImageURL = "https://cdn.pixabay.com/photo/2021/01/01/12/44/concert-5878452_640.jpg";
 
 export const MarketCard = (props: MarketCardProps) => {
 	return (
@@ -14,8 +15,14 @@ export const MarketCard = (props: MarketCardProps) => {
 				src={props.market.imageURL}
 				className="object-cover w-full h-32"
 				alt="market"
+				onError={(e) => {
+					(e.target as HTMLImageElement).src =
+						defaultImageURL;
+				}}
 			/>
-			<p>{props.market.name}</p>
+			<p>
+				<strong>{props.market.name}</strong>
+			</p>
 		</div>
 	);
 };
--- web/share-cart/app/routes/_top/home/index.tsx
+++ web/share-cart/app/routes/_top/home/index.tsx
@@ -14,8 +14,6 @@ export async function loader({ request }: { request: Request }) {
 const testImageURL =
 	"https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80";
 
-const testImageURL2 =
-	"https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg";
 
 export const Home = () => {
 	const { markets } = useLoaderData<typeof loader>();
