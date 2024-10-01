1550fd25f03e62884dc278023a6aa35c81759143
Mkamono <chengyaye31@gmail.com>
2024-10-01 23:30:36 +0900

    update

--- web/share-cart/app/(top)/home/Form.tsx
+++ web/share-cart/app/(top)/home/Form.tsx
@@ -11,7 +11,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -30,7 +29,6 @@ const createMarketFormID = "create-market";
 export function CreateMarketForm({
 	afterSubmit,
 }: { afterSubmit?: () => void } = {}) {
-	const router = useRouter();
 	const form = useForm<z.infer<typeof FormSchema>>({
 		resolver: zodResolver(FormSchema),
 		defaultValues: {
