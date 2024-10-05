7d8f5c93566749af0ca212255f60d97de292be93
Kamono Makoto <77344894+Mkamono@users.noreply.github.com>
2024-10-06 00:54:26 +0900

    feat: 読込中にボタンを無効化する (#107)

--- web/share-cart/app/(top)/market/[id]/DeleteMarketButton.tsx
+++ web/share-cart/app/(top)/market/[id]/DeleteMarketButton.tsx
@@ -3,26 +3,29 @@ import { deleteMarket } from "@/app/actions/deleteMarket";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export function DeleteMarketButton({ id }: { id: string }) {
 	const { toast } = useToast();
+	const [buttonDisabled, setButtonDisabled] = useState(false);
 	const router = useRouter();
 
-	const handleClick = () => {
-		deleteMarket({ id }).then((res) => {
-			if (res.error) {
-				toast({
-					title: "Failed to delete market",
-					description: res.error,
-					variant: "destructive",
-				});
-			} else {
-				router.push("/home");
-			}
-		});
+	const handleClick = async () => {
+		setButtonDisabled(true);
+		const res = await deleteMarket({ id });
+		if (res.error) {
+			toast({
+				title: "Failed to delete market",
+				description: res.error,
+				variant: "destructive",
+			});
+			setButtonDisabled(false);
+		} else {
+			router.push("/home");
+		}
 	};
 	return (
-		<Button variant="destructive" onClick={handleClick}>
+		<Button variant="destructive" disabled={buttonDisabled} onClick={handleClick}>
 			削除
 		</Button>
 	);
