9d7804245c42a52a0d855d446fd14643383e62e9
Mkamono <chengyaye31@gmail.com>
2024-10-05 16:05:09 +0900

    fix style

--- web/share-cart/components/custom-ui/loading-spinner.tsx
+++ web/share-cart/components/custom-ui/loading-spinner.tsx
@@ -1,10 +1,16 @@
-"use client";
-
 import { cn } from "@/lib/utils";
 import * as React from "react";
 
-const spinnerVariants =
-	"w-16 h-16 border-4 border-t-4 border-gray-200 border-t-gray-600 rounded-full animate-spin";
+const spinnerVariants = [
+	"w-16",
+	"h-16",
+	"border-4",
+	"border-t-4",
+	"border-gray-200",
+	"border-t-gray-600",
+	"rounded-full",
+	"animate-spin",
+];
 
 interface LoadingSpinnerProps extends React.HTMLAttributes<HTMLDivElement> {
 	className?: string;
